e46fcc874cc5aeb29e4544bde6a59898860444c9
zfren8 <zfren8@eq.edu.au>
2023-05-12 06:57:22 +1000

    DV VM Prep file updated - v2

--- DRVMPrep.ps1
+++ DRVMPrep.ps1
@@ -2,14 +2,17 @@
 Install-WindowsFeature -Name Hyper-V -IncludeManagementTools
 
 ## Create folder for downlaods
- cd C:\
- mkdir Software
- cd C:\Software
+ cd C:\Users\Intellica\Desktop
 
  $url = "http://cdn.cloudbackup.management/maxdownloads/mxb-rc-windows-x64.exe"
  $ProgressPreference = 'SilentlyContinue'
  Invoke-WebRequest -uri $url -OutFile rc.exe
  $ProgressPreference = "Continue"
+
+## Initialize and format our virtual disk to hold the virtual disaster recover install files
+#get-disk | Where-Object PartitionStyle -eq 'raw' | sort number | Initialize-Disk -PartitionStyle MBR -PassThru | New-Partition -UseMaximumSize -DriveLetter F | Format-Volume -FileSystem NTFS -NewFileSystemLabel "Restore Data" -Confirm
+
+#R
 
 Start-Sleep -Seconds 30
 
