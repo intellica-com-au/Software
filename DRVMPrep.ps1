c03fdf72c1f510dbb0aa7ba70d74987f504d89ac
zfren8 <zfren8@eq.edu.au>
2023-05-12 10:09:42 +1000

    DV VM Prep file updated - v4 Added code to set timezone of machine

--- DRVMPrep.ps1
+++ DRVMPrep.ps1
@@ -1,9 +1,14 @@
 # Install Hyper-V
 Install-WindowsFeature -Name Hyper-V -IncludeManagementTools
 
-## Create folder for downlaods
+#Set our time/zone info
+Set-TimeZone "E. Australia Standard Time"
+Set-WinSystemLocale -SystemLocal en-AU
+
+## Navigate to Intellica Desktop
 Set-Location C:\Users\Intellica\Desktop
 
+# Download RC
  $url = "http://cdn.cloudbackup.management/maxdownloads/mxb-rc-windows-x64.exe"
  $ProgressPreference = 'SilentlyContinue'
  Invoke-WebRequest -uri $url -OutFile rc.exe
@@ -12,8 +17,8 @@ Set-Location C:\Users\Intellica\Desktop
 ## Initialize and format our virtual disk to hold the virtual disaster recover install files
 get-disk | Where-Object PartitionStyle -eq 'raw' | sort number | Initialize-Disk -PartitionStyle MBR -PassThru | New-Partition -UseMaximumSize -DriveLetter F | Format-Volume -FileSystem NTFS -NewFileSystemLabel "Restore Data" -Confirm
 
-#R
+# wait for 60 seconds - ensures disk is initilised and everything is completed before restarting the device
+Start-Sleep -Seconds 60
 
-Start-Sleep -Seconds 30
-
+#Restart Computer
 Restart-Computer
